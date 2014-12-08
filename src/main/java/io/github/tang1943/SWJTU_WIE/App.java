b22c4ab7b70f8f909b4b0d65e136bd972e8eae44
tang1943 <tang1943@gmail.com>
2014-12-08 22:13:16 +0800

    毕设-WEB信息提取 项目建立
    2014年 毕业设计 WEB 信息提取 信息提取网站淘宝

---
+++ src/main/java/io/github/tang1943/SWJTU_WIE/App.java
@@ -0,0 +1,13 @@
+package io.github.tang1943.SWJTU_WIE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
